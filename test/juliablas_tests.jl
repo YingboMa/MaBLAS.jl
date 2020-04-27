53b21485d0b29cb180a92b5d4d4ba5f4db5280db
Yingbo Ma <mayingbo5@gmail.com>
2020-04-27 12:07:29 -0400

    Update tests

--- test/juliablas_tests.jl
+++ test/juliablas_tests.jl
@@ -1,7 +1,16 @@
 using Test
 using SmallLinearAlgebra
+using LinearAlgebra
 
 @testset "Size check" begin
     C = randn(12, 11); A = rand(2, 3); B = rand(2, 3)
     @test_throws DimensionMismatch SmallLinearAlgebra.mul!(C, A, B)
+end
+
+@testset "Kernel sized matmul" begin
+    m, k, n = 8, 83, 30
+    C = rand(m, n)
+    A = rand(m, k)
+    B = rand(k, n)
+    @test SmallLinearAlgebra.mul!((copy(C)), A, B, 2, 3) ≈ LinearAlgebra.mul!((copy(C)), A, B, 2, 3)
 end
