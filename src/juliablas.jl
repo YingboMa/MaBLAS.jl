0634392aa44ca25b1b4c8f5ef953bf2a7aa2df33
Yingbo Ma <mayingbo5@gmail.com>
2020-04-28 23:26:30 -0400

    Add packing_mul

--- src/juliablas.jl
+++ src/juliablas.jl
@@ -18,7 +18,7 @@ using StaticArrays
 ### User-level API
 ###
 
-function mul!(C, A, B, α=true, β=false; cache_params=(cache_m=72, cache_k=256, cache_n=4080))
+function mul!(C, A, B, α=true, β=false; cache_params=(cache_m=72, cache_k=256, cache_n=4080), packing=false)
     iszeroα = iszero(α)
     if iszero(β) && iszeroα
         return fill!(C, zero(eltype(C)))
@@ -35,15 +35,46 @@ function mul!(C, A, B, α=true, β=false; cache_params=(cache_m=72, cache_k=256,
     end
     # assume Float64 for now
     kernel_params = (Val(8), Val(6))
-    return tiling_mul!(C, A, B, α, β, cache_params, kernel_params)
+    if packing
+        packing_mul!(C, A, B, α, β, cache_params, kernel_params)
+    else
+        tiling_mul!(C, A, B, α, β, cache_params, kernel_params)
+    end
+    return C
 end
 
 ###
 ### Lower-level `_mul!`
 ###
 
-function packing_mul!(C, A, B, α=true, β=false)
-
+function packing_mul!(C, A, B, α, β, (cache_m, cache_k, cache_n), kernel_params::Tuple{Val{micro_m},Val{micro_n}}) where {micro_m,micro_n}
+    Abuffer = zeros(cache_m * cache_k)
+    Bbuffer = zeros(cache_k * cache_n)
+    m, k, n = checkmulsize(C, A, B)
+    for cachej₁ in 1:cache_n:n; cachej₂ = min(cachej₁ + cache_n - 1, n)
+        for cachep₁ in 1:cache_k:k; cachep₂ = min(cachep₁ + cache_k - 1, k)
+            _β = cachep₁ == 1 ? β : one(β)
+            packBbuffer!(Bbuffer, B, cachep₁, cachep₂, cachej₁, cachej₂, micro_n)
+            for cachei₁ in 1:cache_m:m; cachei₂ = min(cachei₁ + cache_m - 1, m)
+                packAbuffer!(Abuffer, A, cachei₁, cachei₂, cachep₁, cachep₂, micro_m)
+                # macrokernel
+                for microj₁ in cachej₁:micro_n:cachej₂; nleft = min(cachej₂ - microj₁ + 1, micro_n)
+                    for microi₁ in cachei₁:micro_m:cachei₂; mleft = min(cachei₂ - microi₁ + 1, micro_m)
+                        if nleft == micro_n && mleft == micro_m
+                            # (micro_m × ks) * (ks × micro_n)
+                            # Ĉ = A[i:(i+micro_m-1), ps] * B[ps, j:(j+micro_n-1)]
+                            packing_microkernel!(C, Abuffer, Bbuffer, α, _β, microi₁, microj₁, cachei₁, cachej₁, cachep₁, cachep₂, kernel_params)
+                        else
+                            # (mleft × ks) * (ks × nleft)
+                            # Ĉ = A[i:(i+mleft-1), ps] * B[ps, j:(j+nleft-1)]
+                            #cleanup_packing_microkernel!(C, A, B, α, _β, microi₁, microj₁, cachep₁, cachep₂, mleft, nleft)
+                        end
+                    end # microi
+                end # microj
+            end # cachei
+        end # cachep
+    end # cachej
+    return C
 end
 
 function tiling_mul!(C, A, B, α, β, (cache_m, cache_k, cache_n), kernel_params::Tuple{Val{micro_m},Val{micro_n}}) where {micro_m,micro_n}
@@ -137,6 +168,131 @@ end
 ###
 ### Micro kernel
 ###
+
+@noinline function packing_microkernel!(C::StridedMatrix{Float64}, Abuffer::StridedVector{Float64}, Bbuffer::StridedVector{Float64}, α, β,
+                                        microi₁, microj₁, cachei₁, cachej₁, cachep₁, cachep₂, ::Tuple{Val{micro_m},Val{micro_n}}) where {micro_m,micro_n}
+    T = Float64
+    N = 4
+    V = Vec{N, T}
+    st = sizeof(T)
+    sv = sizeof(V)
+    sc2 = stride(C, 2)*st
+    # we unroll 8 times
+    piters = cachep₂ - cachep₁ + 1
+    punroll, pleft = divrem(piters, 8)
+
+    ptrĈ = getptr(C, microi₁, microj₁) # pointer with offset
+    Ĉ11 = zero(V)
+    Ĉ21 = zero(V)
+    Ĉ12 = zero(V)
+    Ĉ22 = zero(V)
+    Ĉ13 = zero(V)
+    Ĉ23 = zero(V)
+    Ĉ14 = zero(V)
+    Ĉ24 = zero(V)
+    Ĉ15 = zero(V)
+    Ĉ25 = zero(V)
+    Ĉ16 = zero(V)
+    Ĉ26 = zero(V)
+
+    Aoffset = (microi₁ - cachei₁) * piters * st
+    Boffset = (microj₁ - cachej₁) * piters * st
+    ptrÂ = pointer(Abuffer) + Aoffset
+    ptrB̂ = pointer(Bbuffer) + Boffset
+
+    # prefetch C matrix
+    prefetcht0(ptrĈ)
+    prefetcht0(ptrĈ +  sc2)
+    prefetcht0(ptrĈ + 2sc2)
+    prefetcht0(ptrĈ + 3sc2)
+    prefetcht0(ptrĈ + 4sc2)
+    prefetcht0(ptrĈ + 5sc2)
+    # rank-1 updates
+    for _ in 1:punroll
+        prefetcht0(ptrÂ + 8 * st * micro_m)
+        @nexprs 8 u -> begin
+            # iteration u
+            Â1 = vload(V, ptrÂ + (u - 1) * st * micro_m)
+            Â2 = vload(V, ptrÂ + (u - 1) * st * micro_m + sv)
+            #B1..6 = V(@inbounds B[p + (u - 1), j + 0..5])
+            B1 = V(unsafe_load(ptrB̂ + (u - 1) * st * micro_n + 0))
+            Ĉ11 = fma(Â1, B1, Ĉ11)
+            Ĉ21 = fma(Â2, B1, Ĉ21)
+            B2 = V(unsafe_load(ptrB̂ + (u - 1) * st * micro_n + st))
+            Ĉ12 = fma(Â1, B2, Ĉ12)
+            Ĉ22 = fma(Â2, B2, Ĉ22)
+            B3 = V(unsafe_load(ptrB̂ + (u - 1) * st * micro_n + 2st))
+            Ĉ13 = fma(Â1, B3, Ĉ13)
+            Ĉ23 = fma(Â2, B3, Ĉ23)
+            B4 = V(unsafe_load(ptrB̂ + (u - 1) * st * micro_n + 3st))
+            Ĉ14 = fma(Â1, B4, Ĉ14)
+            Ĉ24 = fma(Â2, B4, Ĉ24)
+            B5 = V(unsafe_load(ptrB̂ + (u - 1) * st * micro_n + 4st))
+            Ĉ15 = fma(Â1, B5, Ĉ15)
+            Ĉ25 = fma(Â2, B5, Ĉ25)
+            B6 = V(unsafe_load(ptrB̂ + (u - 1) * st * micro_n + 5st))
+            Ĉ16 = fma(Â1, B6, Ĉ16)
+            Ĉ26 = fma(Â2, B6, Ĉ26)
+        end
+        ptrÂ += 8 * st * micro_m
+        ptrB̂ += 8 * st * micro_n
+    end
+
+    for _ in 1:pleft
+        Â1 = vload(V, ptrÂ)
+        Â2 = vload(V, ptrÂ + sv)
+        B1 = V(unsafe_load(ptrB̂))
+        Ĉ11 = fma(Â1, B1, Ĉ11)
+        Ĉ21 = fma(Â2, B1, Ĉ21)
+        B2 = V(unsafe_load(ptrB̂ + st))
+        Ĉ12 = fma(Â1, B2, Ĉ12)
+        Ĉ22 = fma(Â2, B2, Ĉ22)
+        B3 = V(unsafe_load(ptrB̂ + 2st))
+        Ĉ13 = fma(Â1, B3, Ĉ13)
+        Ĉ23 = fma(Â2, B3, Ĉ23)
+        B4 = V(unsafe_load(ptrB̂ + 3st))
+        Ĉ14 = fma(Â1, B4, Ĉ14)
+        Ĉ24 = fma(Â2, B4, Ĉ24)
+        B5 = V(unsafe_load(ptrB̂ + 4st))
+        Ĉ15 = fma(Â1, B5, Ĉ15)
+        Ĉ25 = fma(Â2, B5, Ĉ25)
+        B6 = V(unsafe_load(ptrB̂ + 5st))
+        Ĉ16 = fma(Â1, B6, Ĉ16)
+        Ĉ26 = fma(Â2, B6, Ĉ26)
+        ptrÂ += st * micro_m
+        ptrB̂ += st * micro_n
+    end
+
+    if iszero(β)
+        vecstore(α * Ĉ11, C, ptrĈ, 1, 1)
+        vecstore(α * Ĉ21, C, ptrĈ, 2, 1)
+        vecstore(α * Ĉ12, C, ptrĈ, 1, 2)
+        vecstore(α * Ĉ22, C, ptrĈ, 2, 2)
+        vecstore(α * Ĉ13, C, ptrĈ, 1, 3)
+        vecstore(α * Ĉ23, C, ptrĈ, 2, 3)
+        vecstore(α * Ĉ14, C, ptrĈ, 1, 4)
+        vecstore(α * Ĉ24, C, ptrĈ, 2, 4)
+        vecstore(α * Ĉ15, C, ptrĈ, 1, 5)
+        vecstore(α * Ĉ25, C, ptrĈ, 2, 5)
+        vecstore(α * Ĉ16, C, ptrĈ, 1, 6)
+        vecstore(α * Ĉ26, C, ptrĈ, 2, 6)
+    else
+        vecstore(fma(α, Ĉ11, β * vecload(V, C, ptrĈ, 1, 1)), C, ptrĈ, 1, 1)
+        vecstore(fma(α, Ĉ21, β * vecload(V, C, ptrĈ, 2, 1)), C, ptrĈ, 2, 1)
+        vecstore(fma(α, Ĉ12, β * vecload(V, C, ptrĈ, 1, 2)), C, ptrĈ, 1, 2)
+        vecstore(fma(α, Ĉ22, β * vecload(V, C, ptrĈ, 2, 2)), C, ptrĈ, 2, 2)
+        vecstore(fma(α, Ĉ13, β * vecload(V, C, ptrĈ, 1, 3)), C, ptrĈ, 1, 3)
+        vecstore(fma(α, Ĉ23, β * vecload(V, C, ptrĈ, 2, 3)), C, ptrĈ, 2, 3)
+        vecstore(fma(α, Ĉ14, β * vecload(V, C, ptrĈ, 1, 4)), C, ptrĈ, 1, 4)
+        vecstore(fma(α, Ĉ24, β * vecload(V, C, ptrĈ, 2, 4)), C, ptrĈ, 2, 4)
+        vecstore(fma(α, Ĉ15, β * vecload(V, C, ptrĈ, 1, 5)), C, ptrĈ, 1, 5)
+        vecstore(fma(α, Ĉ25, β * vecload(V, C, ptrĈ, 2, 5)), C, ptrĈ, 2, 5)
+        vecstore(fma(α, Ĉ16, β * vecload(V, C, ptrĈ, 1, 6)), C, ptrĈ, 1, 6)
+        vecstore(fma(α, Ĉ26, β * vecload(V, C, ptrĈ, 2, 6)), C, ptrĈ, 2, 6)
+    end
+
+    return nothing
+end
 
 # (micro_m × ks) * (ks × micro_n)
 # Ĉ = A[i:(i+micro_m-1), ps] * B[ps, j:(j+micro_n-1)]
