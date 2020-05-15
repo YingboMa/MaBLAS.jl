5d453dd754c93ad129c564a1d30c0148296481da
Yingbo Ma <mayingbo5@gmail.com>
2020-05-15 01:03:13 -0400

    Better tests

--- test/gemm_tests.jl
+++ test/gemm_tests.jl
@@ -23,6 +23,9 @@ end
             @test MaBLAS.mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb)) ≈ LinearAlgebra.mul!((copy(C)), A, B, α, β)
         end
     end
+    packa, packb = true, false
+    α, β = randn(2)
+    @test MaBLAS.mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb)) == MaBLAS.mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(Val(packa), Val(packb)))
 end
 
 @testset "Block size tests" begin
@@ -35,15 +38,17 @@ end
     _m, _k, _n = 8*3, 8, 6*5
     # lower cache_params to check clean up loops more easily
     cache_params = (cache_m=_m, cache_k=_k, cache_n=_n)
-    m, k, n = 73, 131, 257 # all prime numbers
-    C = rand(m, n)
-    A = rand(m, k)
-    B = rand(k, n)
-    for α in (1, 2, 3, false, true), β in (1, 2, 3, false, true), packa in (true, false), packb in (true, false)
-        for kernel_params in [(Val(8), Val(6)), (Val(12), Val(4)), (Val(4), Val(3))]
-            @test MaBLAS.mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb)) ≈ LinearAlgebra.mul!((copy(C)), A, B, α, β)
-            @allocated noreturn_mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb))
-            @allocated(noreturn_mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb))) <= (packa + packb) * 176 # minor allocation when packing
+    α, β = randn(2)
+    for T in (Float64, Float32)
+        for packa in (true, false), packb in (true, false), (m, k, n) in [(73, 131, 257), (101, 103, 107), (109, 113, 127), (131, 137, 139), (149, 151, 157), (163, 167, 173), (179, 181, 191), (193, 197, 199)] # all primes
+            for kernel_params in [(Val(8), Val(6)), (Val(12), Val(4)), (Val(4), Val(3))]
+                C = rand(T, m, n)
+                A = rand(T, m, k)
+                B = rand(T, k, n)
+                @test MaBLAS.mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb)) ≈ LinearAlgebra.mul!((copy(C)), A, B, α, β)
+                @allocated noreturn_mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb))
+                @allocated(noreturn_mul!((copy(C)), A, B, α, β; cache_params=cache_params, packing=(packa, packb))) <= (packa + packb) * 176 # minor allocation when packing
+            end
         end
     end
 end
@@ -83,7 +88,7 @@ end
     timer = MaBLAS.get_timer()
     MaBLAS.enable_timer()
     MaBLAS.reset_timer!()
-    for α in (1, 2, 3, false, true), β in (1, 2, 3, false, true), A in (rand(m, k), rand(k, m)'), B in (rand(k, n), rand(n, k)'), packa in (true, false), packb in (true, false)
+    for α in (1, 2, 3.0, false, true), β in (1, 2, 3.0, false, true), A in (rand(m, k), rand(k, m)'), B in (rand(k, n), rand(n, k)'), packa in (true, false), packb in (true, false)
         try
             AB = MaBLAS.mul!((copy(C)), A, B, α, β; packing=(packa, packb))
             @test AB ≈ LinearAlgebra.mul!((copy(C)), A, B, α, β)
@@ -98,7 +103,9 @@ end
 
     MaBLAS.reset_timer!()
     MaBLAS.disable_timer()
-    for α in (1, 2, 3, false, true), β in (1, 2, 3, false, true), A in (rand(m, k), rand(k, m)'), B in (rand(k, n), rand(n, k)')
+
+    α, β = rand(2)
+    for A in (rand(m, k), rand(k, m)', PermutedDimsArray(rand(k, m), (2, 1))), B in (rand(k, n), rand(n, k)', PermutedDimsArray(rand(n, k), (2, 1)))
         MaBLAS.mul!((copy(C)), A, B, α, β; packing=(true, true)) ≈ LinearAlgebra.mul!((copy(C)), A, B, α, β)
     end
     display(timer)
